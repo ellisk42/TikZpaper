9f78de5bcb324890336fd3276fab4bf1d4d787fc
Kevin Ellis <ellisk@mit.edu>
2018-10-26 13:41:23 -0400

    fixed figures

--- makeArchive.sh
+++ makeArchive.sh
@@ -4,7 +4,9 @@ rm -r archive archive.tar
 mkdir archive
 mkdir archive/
 
-cp long.tex main.bib long.bbl nips_2018.sty /usr/share/texlive/texmf-dist/tex/latex/natbib/natbib.sty archive
+cp long.tex main.bib long.bbl nips_2018.sty archive
+#cp /usr/share/texlive/texmf-dist/tex/latex/natbib/natbib.sty archive
+cp natbib.sty archive
 
 cp --parents `grep png long.tex|sed 's/.*figures/figures/'|sed 's/}.*//'` archive/
 cp figures/expert-*-trim.png archive/figures
